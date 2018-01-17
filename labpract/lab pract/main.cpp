a59abbf83568d849db25a8994ea1c250a2ab93ef
noah Branch <noahbranch@noahs-MacBook-Pro.local>
2018-01-17 13:19:13 -0500

    first m

---
+++ labpract/lab pract/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab pract
+//
+//  Created by noah Branch on 1/17/18.
+//  Copyright © 2018 noah Branch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
